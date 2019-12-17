b34d72094b42eb355a161fe7be5f5bfffc8ad5cb
STbreak <stb@inl.ics.keio.ac.jp>
2019-12-17 16:19:46 +0900

    評価用実装完了

--- namekuji/CS/GenID.py
+++ namekuji/CS/GenID.py
@@ -2,10 +2,12 @@
 import socket
 import pdb
 import random
+import binascii
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 
-
+#GCIDリストのパス設定
+path = "./GCID_List.txt"
 
 '''
 <<Step1：GCIDの作成>>
@@ -24,14 +26,15 @@ gcid_upper = random.randrange(2 ** 128).to_bytes(len_gcid_upper, 'big')
 private_key = RSA.generate(2048)
 len_private = len(private_key.export_key('DER')) #len_private：秘密鍵のbyte長
 
-#秘密鍵に基づく公開鍵の生成
-public_key = private_key.publickey()
-
 #GCID(bytes)の作成
 GCID_int = (int.from_bytes(gcid_upper, 'big') << (len_private * 8)) | int.from_bytes(private_key.export_key('DER'), 'big') #gcid_upperの後に秘密鍵をくっつける
 GCID = GCID_int.to_bytes(len_gcid_upper + len_private, 'big')
 len_gcid = len(GCID)
 
+
+#GCIDを文字列としてリストに格納
+with open(path, mode='a') as f:
+    f.write(GCID.hex() + '\n')
 #Step1の完了
 
 print("Done：GCID生成\n")
@@ -74,57 +77,74 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 ・合わせて288byteになる
 '''
 
-#VCLの上位128bitの作成
-len_vcid_upper = 16
-vcid_upper = random.randrange(2 ** 128).to_bytes(len_vcid_upper, 'big')
+#元のGCIDをランダムに選択
+#GCIDをファイルから読み出す
+with open(path, mode='r') as f:
+    GCID_List = f.read().split('\n')
 
-#シーケンス（とりあえず128bit）の作成
-len_sequence = 16
-sequence = random.randrange(2 ** (len_sequence * 8)).to_bytes(16, 'big')
+for i in range(len(GCID_List) - 1):
+    GCID = GCID_List[random.randrange(len(GCID_List) - 1)]
+    #GCIDの型をbytesに戻す
+    GCID = binascii.unhexlify(GCID)
+    #GCIDから秘密鍵を取り出す
+    len_gcid = len(GCID)
+    len_private = len_gcid - len_gcid_upper
+    private_key_int = ((int.from_bytes(GCID, 'big') << (len_gcid_upper * 8)) & (2 ** (len_gcid * 8) - 1)) >> (len_gcid_upper * 8)
+    private_key = private_key_int.to_bytes(len_private, 'big')
 
-#平文（上位128bitとシーケンスの合計）の作成
-len_vcid_plain = len_vcid_upper + len_sequence
-vcid_plain_int = (int.from_bytes(vcid_upper, 'big') << (len_sequence * 8)) | int.from_bytes(sequence, 'big') #vcid_plain（int型）の作成)
-vcid_plain = vcid_plain_int.to_bytes(len_vcid_plain, 'big')
+    #秘密鍵の属性をRSAkeyに変換
+    private_key = RSA.import_key(private_key, passphrase=None)
 
-#暗号文の作成
-cipher = PKCS1_OAEP.new(public_key)
-ciphertext = cipher.encrypt(vcid_plain)
-len_ciphertext = len(ciphertext)
+    #公開鍵を作成
+    public_key = private_key.publickey()
 
-#VCLの作成
-VCID_int = (int.from_bytes(vcid_plain, 'big') << (len_ciphertext * 8)) | int.from_bytes(ciphertext, 'big')
-VCID = VCID_int.to_bytes(len_vcid_plain + len_ciphertext, 'big')
-len_vcid = len(VCID)
+    #VCLの上位128bitの作成
+    len_vcid_upper = 16
+    vcid_upper = random.randrange(2 ** 128).to_bytes(len_vcid_upper, 'big')
 
-#Step2の完了
+    #シーケンス（とりあえず128bit）の作成
+    len_sequence = 16
+    sequence = random.randrange(2 ** (len_sequence * 8)).to_bytes(16, 'big')
 
-print("Done：VCID生成\n")
-print(len(VCID))
+    #平文（上位128bitとシーケンスの合計）の作成
+    len_vcid_plain = len_vcid_upper + len_sequence
+    vcid_plain_int = (int.from_bytes(vcid_upper, 'big') << (len_sequence * 8)) | int.from_bytes(sequence, 'big') #vcid_plain（int型）の作成)
+    vcid_plain = vcid_plain_int.to_bytes(len_vcid_plain, 'big')
 
+    #暗号文の作成
+    cipher = PKCS1_OAEP.new(public_key)
+    ciphertext = cipher.encrypt(vcid_plain)
+    len_ciphertext = len(ciphertext)
 
-#ClientにVCIDを渡す
+    #VCLの作成
+    VCID_int = (int.from_bytes(vcid_plain, 'big') << (len_ciphertext * 8)) | int.from_bytes(ciphertext, 'big')
+    VCID = VCID_int.to_bytes(len_vcid_plain + len_ciphertext, 'big')
+    len_vcid = len(VCID)
 
+    #Step2の完了
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    # IPアドレスとポートを指定
-    s.bind(('127.0.0.1', 10001))
-    # 1 接続
-    s.listen(1)
-    # connectionするまで待つ
-    while True:
-        # connectionがあればコネクションとアドレスを入れる
-        conn, addr = s.accept()
-        with conn:
-            while True:
-                #データを受け取る
-                data = conn.recv(2048)
-                if not data:
+    print("Done：VCID生成\n")
+
+    #ClientにVCIDを渡す
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        # IPアドレスとポートを指定
+        s.bind(('127.0.0.1', 10001 + i))
+        # 1 接続
+        s.listen(1)
+        # connectionするまで待つ
+        while True:
+            # connectionがあればコネクションとアドレスを入れる
+            conn, addr = s.accept()
+            with conn:
+                while True:
+                    #データを受け取る
+                    data = conn.recv(2048)
+                    if not data:
+                        break
+                    print('data : {}, addr: {}'.format(data, addr))
+                    conn.sendall(VCID)
                     break
-                print('data : {}, addr: {}'.format(data, addr))
-                conn.sendall(VCID)
-                break
-        break
+            break
 
 
 '''
